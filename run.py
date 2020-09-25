213d7d4d0b6df765996c56f2388bd472fb2e0081
Guilherme Fickel <guilhermefickel@gmail.com>
2020-09-25 15:34:51 -0300

    Using PIL instead of cv2

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import time
-import cv2
 import numpy as np
+from PIL import Image
 
 from contrast_image import contrast_image
 from naive_contrast_image import naive_contrast_image
 
-im = cv2.imread('images/turing.jpg', -1)
+im = Image.open('images/turing.jpg')
+im = np.array(im)
 
 begin = time.time()
 for i in range(10):
@@ -17,9 +18,9 @@ for i in range(10):
     contrast_im = naive_contrast_image(im)
 print('Mean runtime naive', (time.time()-begin)*1000/10, 'ms')
 
-cv2.imwrite('images/turing_contrast.png', contrast_im)
-
 comb = np.zeros((im.shape[0],im.shape[1]*2), dtype=np.uint8)
 comb[:,0:im.shape[1]] = im
 comb[:,im.shape[1]:] = contrast_im
-cv2.imwrite('images/turing_gal.png', comb)
+
+comb_im = Image.fromarray(comb)
+comb_im.save('images/turing_gal.png')
